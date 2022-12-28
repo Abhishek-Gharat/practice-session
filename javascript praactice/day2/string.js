de9c149a655ba0fc8ef9db1740381c28ea9c3280
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2022-12-28 23:57:19 +0530

    replace

--- javascript praactice/day2/string.js
+++ javascript praactice/day2/string.js
@@ -1,3 +1,17 @@
 // what will the following print in javascript
 let str="abhishek\""
 console.log(str.length);
+// upparcase
+let str2="gharat";
+let result=str2.toUpperCase();
+console.log(result);
+// etract word
+let word="please send me 1000"
+let op=word.slice(15);
+console.log(op);
+
+
+// change 4th char
+let chng="i have learn some skills today"
+let change=chng.replace("some","more")
+console.log(change);
